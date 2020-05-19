682cc1584b7809a9f965f77fa47bc5d611b6f9ce
kasinbadee wongthongluea <kasinbadee@chaostheory@asia>
2020-05-19 13:14:07 +0700

    update new version string2int Challenge

--- string2intChallenge.py
+++ string2intChallenge.py
@@ -3,17 +3,42 @@
 #Python version 3.6 ++
 
 def checkInt(input):
-	list_INT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-	strInt = ''
+	list_strInt = []
+	dict_str2int = {'0': 0,
+					'1': 1,
+					'2': 2,
+					'3': 3,
+					'4': 4,
+					'5': 5,
+					'6': 6,
+					'7': 7,
+					'8': 8,
+					'9': 9
+					}
+	for x in range(0,len(input)):
+		for k, v in dict_str2int.items():
+			#check digit str to int from list
+			if input[x] == k:
+				list_strInt.append(v)
 
-	#loop string input 
-	for x in input:
-		#check number string.
-		for y in list_INT:
-			if x == y:
-				strInt += x
+	#set output type int 
+	output = 0
 
-	return int(strInt)
+	for x in range(0,len(list_strInt)):
+		#set cost start -> 10
+		set_int = 10
+
+		#calculate loop position number
+		for y in range(0,len(list_strInt)-x-2):
+			set_int *= 10
+
+		#check last position number calculator set cost 10 to 1
+		if x+1 == len(list_strInt):
+			set_int = 1
+
+		output += list_strInt[x] * set_int
+		
+	return output
 
 
 #Start Program
